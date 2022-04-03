0871ffd678ca10435fe5fb6c408edec83f82de60
Saphire <lattice@saphi.re>
2022-04-04 03:49:15 +0700

    Add template image cachebusting

--- overlayScript.user.js
+++ overlayScript.user.js
@@ -10,7 +10,6 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-
 async function get(url, parseJson = true, headers = {}) {
     return new Promise((resolve, reject) => {
         let xhr = GM_xmlhttpRequest({
@@ -18,9 +17,12 @@ async function get(url, parseJson = true, headers = {}) {
             url,
             onload: (r) => resolve(parseJson ? JSON.parse(r.response) : r.response),
             onerror: (e) => reject(e),
-            headers: headers ?? parseJson ? {
-                "Accept": "application/json"
-            } : {}
+            headers:
+                headers ?? parseJson
+                    ? {
+                          Accept: "application/json",
+                      }
+                    : {},
         });
     });
 }
@@ -33,17 +35,20 @@ class PlaceOverlay {
 
     overlays = [];
 
+    overlayCanvas = document.createElement("canvas");
     overlaysContainer = document.createElement("div");
     toggleButton = document.createElement("div");
     modalContainer = document.createElement("div");
     modalMain = document.createElement("div");
+
+    offscreenCanvas = document.createElement("canvas");
 
     placeRoot = null;
 
     tempToggle = false;
 
     constructor() {
-        window.addEventListener('load', () => this.init());
+        window.addEventListener("load", () => this.init());
     }
 
     async init() {
@@ -68,26 +73,32 @@ class PlaceOverlay {
     }
 
     async updateOverlays() {
-        this.overlays.forEach(o => o.elem.remove());
+        this.overlays.forEach((o) => o.elem.remove());
 
         const data = await get(this.repoRoot + `overlays.json?cachebust=${new Date().getTime()}`);
 
         if (data.scriptVersion != this.scriptVersion) {
             console.log(`Version mismatch, got "${data.scriptVersion}", expected "${this.scriptVersion}"`);
-            this.modalMain.innerText = "Your template script is out of date! Reload the page or update the script";
+            this.modalMain.innerText =
+                "Your template script is out of date! Reload the page or update the script";
             this.modalContainer.style.display = "flex";
         }
 
-        this.overlays = data.overlays.map(
-            meta => {
-                return {
-                    meta,
-                    elem: this.makeOverlayImage(meta.url, meta.posX, meta.posY, meta.width ?? -1, meta.height ?? -1, meta.scale ?? 3)
-                }
-            }
-        );
+        this.overlays = data.overlays.map((meta) => {
+            return {
+                meta,
+                elem: this.makeOverlayImage(
+                    meta.url,
+                    meta.posX,
+                    meta.posY,
+                    meta.width ?? -1,
+                    meta.height ?? -1,
+                    meta.scale ?? 3
+                ),
+            };
+        });
 
-        this.overlays.forEach(o => this.overlaysContainer.appendChild(o.elem));
+        this.overlays.forEach((o) => this.overlaysContainer.appendChild(o.elem));
     }
 
     domInit() {
@@ -96,36 +107,33 @@ class PlaceOverlay {
         this.setStyles();
 
         // when a key is being held down, we get multiple onkeydown events, so we need to track that
-        window.addEventListener("keydown",
-            (event) => {
-                if ((event.key === "h") && !this.tempToggle) {
-                    this.tempToggle = true;
-                    this.toggle();
-                }
+        window.addEventListener("keydown", (event) => {
+            if (event.key === "h" && !this.tempToggle) {
+                this.tempToggle = true;
+                this.toggle();
             }
-        );
-        window.addEventListener("keyup",
-            event => {
-                if ((event.key === "h") && this.tempToggle) {
-                    this.tempToggle = false;
-                    this.toggle();
-                }
+        });
+        window.addEventListener("keyup", (event) => {
+            if (event.key === "h" && this.tempToggle) {
+                this.tempToggle = false;
+                this.toggle();
             }
-        )
+        });
         this.toggleButton.addEventListener("click", () => this.toggle());
 
         this.modalContainer.appendChild(this.modalMain);
         document.querySelector("body").appendChild(this.modalContainer);
 
-        this.placeRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(this.overlaysContainer);
+        this.placeRoot.children[0]
+            .getElementsByTagName("mona-lisa-canvas")[0]
+            .shadowRoot.children[0].appendChild(this.overlaysContainer);
 
         this.updateButton();
         this.placeRoot.querySelector(".bottom-controls").appendChild(this.toggleButton);
     }
 
     setStyles() {
-        this.toggleButton.style =
-            `height: 24px;
+        this.toggleButton.style = `height: 24px;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -164,28 +172,31 @@ class PlaceOverlay {
             box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 10px;
             font-family: var(--mona-lisa-font-sans);
             padding: 1em 2em;
-            pointer-events: auto;`
+            pointer-events: auto;`;
     }
 
     makeOverlayImage(src, x = 0, y = 0, w = 2000, h = 1000, scale = 3) {
         const imgElem = document.createElement("img");
+        if (src.includes("https://")) {
+            const hasQuery = src.includes("?");
+            const endsAnd = src.endsWith("&");
+
+            src = `${src}${hasQuery ? (endsAnd ? "" : "&") : "?"}cachebust=${new Date().getTime()}`;
+        }
         imgElem.src = src;
         imgElem.addEventListener("load", () => {
-            if (w == -1)
-                w = imgElem.width / scale;
-            if (h == -1)
-                h = imgElem.height / scale;
+            if (w == -1) w = imgElem.width / scale;
+            if (h == -1) h = imgElem.height / scale;
             imgElem.style = `position: absolute;
                 left: ${x};
                 top: ${y};
                 image-rendering: pixelated;
                 width: ${w}px;
-                height: ${h}px;`
-        })
+                height: ${h}px;`;
+        });
         return imgElem;
     }
 }
-
 
 if (window.top !== window.self) {
     new PlaceOverlay();
